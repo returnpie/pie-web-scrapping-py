907e22856f0897e2e315b3ba851982187914fff3
pie <returnpie@icloud.com>
2020-06-11 00:03:14 +0900

    get page number span tag

--- main.py
+++ main.py
@@ -5,4 +5,21 @@ indeed_resul = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
 
 indeed_soup = BeautifulSoup(indeed_resul.text, 'html.parser')
 
-print(indeed_soup)
+
+pagination = indeed_soup.find("div", { "class" : "pagination"})
+
+# print(pagination)
+
+pages = pagination.find_all('a')
+
+# print(pages)
+
+spans = []
+
+for page in pages:
+    spans.append(page.find("span"))
+    # print(page.find("span"))
+
+spans = spans[:-1]
+
+print(spans)
