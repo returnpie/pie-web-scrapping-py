01905620e6a2278f8389f1448b8dac2b9bf067ea
pie <returnpie@icloud.com>
2020-06-15 01:26:07 +0900

    get title, company from indeed

--- indeed.py
+++ indeed.py
@@ -22,12 +22,19 @@ def extract_indeed_pages():
 
 def extract_indeed_jobs(last_page):
     # for page in range(last_page):
-    resul = requests.get(f"{URL}&start={0*LIMIT}")
-    soup = BeautifulSoup(resul.text, 'html.parser')
+    job_list = requests.get(f"{URL}&start={0*LIMIT}")
+    soup = BeautifulSoup(job_list.text, 'html.parser')
     results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
 
     for result in results:
-        title = result.find("div", {"class": "title"})
-        # print(title.find("a"))
-        print(title)
-    print(results)
+        title = result.find("h2", {"class": "title"}).find("a")["title"]
+        company = result.find("span", {"class": "company"})
+        company_anchor = company.find("a")
+        if company_anchor is not None:
+            company = (str(company_anchor.string))
+        else:
+            company = (str(company.string))
+        # print(company)
+        company = company.strip()
+        print(title, company)
+    # print(results)
