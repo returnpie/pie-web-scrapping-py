81d0a6b32a30c93e84ace67fe86342fd7a58c63b
pie <returnpie@icloud.com>
2020-06-16 23:58:30 +0900

    get location, url

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-from indeed import extract_indeed_pages, extract_indeed_jobs
+from indeed import get_jobs as get_indeed_jobs
 
-last_indeed_page = extract_indeed_pages()
-
-# print(max_indeed_pages)
-
-extract_indeed_jobs(last_indeed_page)
