29795e4183d383bba28baae26a057b9a78fae8e1
pie <returnpie@icloud.com>
2020-06-18 00:20:20 +0900

    complete stackoverflow

--- so.py
+++ so.py
@@ -12,15 +12,26 @@ def get_last_page():
     return int(last_page)
 
 
+def extract_job(html):
+    title = html.find("a", {"class": "s-link"})["title"]
+    company, location = html.find(
+        "h3", {"class": "fc-black-700"}).find_all("span", recursive=False)
+    company = company.get_text(strip=True)
+    location = location.get_text(strip=True)
+    job_id = html['data-jobid']
+    return {'title': title, 'company': company, 'location': location, "apply_link": f"https://stackoverflow.com/jobs/{job_id}"}
+
+
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        print(f"scarapping so: Page: {page}")
         result = requests.get(f"{URL}&pg={page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
         for result in results:
-            print(result["data-jobid"])
-        # print(result.status_code)
+            job = extract_job(result)
+            jobs.append(job)
     return jobs
 
 
