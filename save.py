209796daa839e35e4598ceaf1dc39fd596f6c2ed
pie <returnpie@icloud.com>
2020-06-19 01:49:47 +0900

    complete save

--- save.py
+++ save.py
@@ -3,5 +3,9 @@ import csv
 
 def save_to_file(jobs):
     file = open("jobs.csv", mode="w")
+    writer = csv.writer(file)
+    writer.writerow(["title", " company", " location", " link"])
+    for job in jobs:
+        writer.writerow(list(job.values()))
     print(file)
     return
